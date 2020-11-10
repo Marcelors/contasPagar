8cb078485dbd3cd6c0492ff5d6781c0f0f3c0553
Marcelo Rodrigues Silva <marcelo.rodrigues@teckavan.com>
2020-11-10 16:13:03 -0300

    Commit inicial

---
+++ BILLSToPAY.Domain.Shared/Models/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BILLSToPAY.Domain.Shared.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        public void NewId()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public void SetId(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
