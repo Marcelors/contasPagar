3389595d90d490427a0a7185286bfcfa7d8833fd
Marcelo Rodrigues Silva <marcelo.rodrigues@teckavan.com>
2020-11-12 11:05:38 -0300

    criando os migrations

---
+++ src/api/BILLSToPAY.Infra.Data/Migrations/20201112134429_initial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BILLSToPAY.Infra.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rule",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Days = table.Column<int>(nullable: false),
+                    Type = table.Column<short>(nullable: false),
+                    InterestPerDay = table.Column<decimal>(type: "decimal(18, 6)", nullable: false),
+                    Penalty = table.Column<decimal>(type: "decimal(18, 6)", nullable: false),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rule", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    OriginalValue = table.Column<decimal>(type: "decimal(18, 6)", nullable: false),
+                    PaymentDate = table.Column<DateTime>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    CorrectedValue = table.Column<decimal>(type: "decimal(18, 6)", nullable: false),
+                    NumberOfDaysLate = table.Column<int>(nullable: false),
+                    RuleId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_Rule_RuleId",
+                        column: x => x.RuleId,
+                        principalTable: "Rule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RuleId",
+                table: "Account",
+                column: "RuleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Rule");
+        }
+    }
+}
---
+++ src/api/BILLSToPAY.Infra.Data/Migrations/20201112134815_insert_rules_default.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BILLSToPAY.Infra.Data.Migrations
+{
+    public partial class insert_rules_default : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO [RULE](ID, Days, Type, InterestPerDay, Penalty, Active)
+                                   values (NEWID(), 3, 1, 0.1, 2, 1)");
+            migrationBuilder.Sql(@"INSERT INTO [RULE](ID, Days, Type, InterestPerDay, Penalty, Active)
+                                   values (NEWID(), 3, 2, 0.2, 3, 1)");
+            migrationBuilder.Sql(@"INSERT INTO [RULE](ID, Days, Type, InterestPerDay, Penalty, Active)
+                                   values (NEWID(), 5, 2, 0.3, 5, 1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM RULE");
+        }
+    }
+}
